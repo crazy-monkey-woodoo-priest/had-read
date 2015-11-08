bc4006a477b8b4922eab109ec456df1181fd432e
Tomas Valent <equivalent@eq8.eu>
2015-11-08 22:58:07 +0000

    fix specs

--- spec/lib/reading_log_processer_spec.rb
+++ spec/lib/reading_log_processer_spec.rb
@@ -113,3 +113,21 @@ RSpec.describe ReadingLogProcessor do
     end
   end
 end
+
+RSpec.describe ReadingLogExtractor::Processor, integration: true, vcr: { cassette_name: "github-repo-list" } do
+  subject { described_class.new(username: username, gh_facade: gh_facade) }
+
+  let(:gh_connection) { Github }
+  let(:gh_facade) { ReadingLogExtractor::GithubFacade.new(gh_connection: gh_connection) }
+
+  describe '#repo_exist?' do
+    context 'Given user with valid reading-log repo' do
+
+      let(:username) { 'equivalent' }
+
+      it 'should return true' do
+        expect(subject.repo_exist?).to be true
+      end
+    end
+  end
+end
